de97ce5c37b5a9e0a0cb807721dd00c165320877
talip <talipefe35@gmail.com>
2021-04-25 15:08:09 +0300

    değişim

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-         primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -22,53 +21,108 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-final String title;
+  final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  StreamController _streamController=StreamController();
-
+  int _counter = 0;
   AudioPlayer audioPlayer=AudioPlayer();
+  Duration duration;
 
-    void streamYayin() async{
-      for(int i=0; i<10; i++){
-       // if(i==6){_streamController.addError('bir hata oluştu');}
-        await Future.delayed(Duration(seconds: 1));
-        _streamController.sink.add(i+1);
-      }
-      _streamController.close();
+  play() async{
+    await audioPlayer.setUrl("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3");
+    int result=await audioPlayer.play("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3");
+    if(result==1){
+      print('Play');
     }
+  }
 
+  stop() async{
+    int result=await audioPlayer.stop();
+    if(result==1){
+      print('Stop');
+    }
+  }
+
+  pause() async{
+    int result=await audioPlayer.pause();
+    if(result==1){
+      print('Pause');
+    }
+  }
+
+  seek() async{
+    int result=await audioPlayer.seek(Duration(seconds:350));
+  }
+
+  duration1(){
+    audioPlayer. onDurationChanged.listen((Duration d) {
+      print('Max duration:$d');
+      setState(() {
+        duration=d;
+      });
+    });
+  }
+
+  position1(){
+  audioPlayer.onAudioPositionChanged.listen((Duration p) {
+    print('Current position :$p');
+  });
+  }
+
+  durum(){
+       audioPlayer.onPlayerStateChanged.listen((AudioPlayerState s) {
+         print(s);
+       });
+       }
+
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
-         child: Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ElevatedButton(
-                onPressed:null,
-                child: Text('Yayınla')
+                onPressed: play,
+                child: Text('Play')
             ),
             ElevatedButton(
-                onPressed: () {
-                  streamYayin();
-     _streamController.stream.listen((event){
-       print(event*10);},
-       onDone: (){
-       print('Stream bitti');
-       },
-       cancelOnError: true
-       );},
-                child: Text('Reciever')
+                onPressed: pause,
+                child: Text('Pause')
+            ),
+            ElevatedButton(
+                onPressed: stop,
+                child: Text('Stop')
+            ),
+            ElevatedButton(
+                onPressed: seek,
+                child: Text('Seek')
+            ),
+            ElevatedButton(
+                onPressed: durum,
+                child: Text('Durum')
+            ),
+            ElevatedButton(
+                onPressed: duration1,
+                child: Text('Duration Event')
+            ),
+            ElevatedButton(
+                onPressed: position1,
+                child: Text('Position Event')
             ),
           ],
         ),
@@ -77,8 +131,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-
-
-
-
---
+++ lib/yedek02.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  StreamController _streamController = StreamController();
+
+  AudioPlayer audioPlayer = AudioPlayer();
+
+  void streamYayin() async {
+    for (int i = 0; i < 10; i++) {
+      // if(i==6){_streamController.addError('bir hata oluştu');}
+      await Future.delayed(Duration(seconds: 1));
+      _streamController.sink.add(i + 1);
+    }
+    _streamController.close();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(onPressed: null, child: Text('Yayınla')),
+            ElevatedButton(
+                onPressed: () {
+                  streamYayin();
+                  StreamSubscription streamSubscription;
+                  streamSubscription=_streamController.stream.listen((event) {
+                    print(event * 10);
+                  }, onDone: () {
+                    print('Stream bitti');
+                  }, cancelOnError: true
+                  );// to listen
+                  // streamSubscription.cancel();
+                },
+                child: Text('Reciever')),
+          ],
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
